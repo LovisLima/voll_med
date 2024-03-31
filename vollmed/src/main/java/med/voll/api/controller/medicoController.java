29d80f36afabbe32b44b98b9ea02d3edf1794f0b
wagner lima <wagner.lima.andrade@alumni.usp.br>
2024-03-31 16:57:06 -0300

    feat: medicoController, ClasseMedico, MedicoRepository, ClasseEndereço

--- vollmed/src/main/java/med/voll/api/controller/medicoController.java
+++ vollmed/src/main/java/med/voll/api/controller/medicoController.java
@@ -1,17 +1,26 @@
 package med.voll.api.controller;
 
+import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("medicos")
 public class medicoController {
+
+    @Autowired
+    private MedicoRepository repository;
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico dados){
-        System.out.println(dados);
+    public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados){
+        repository.save(new Medico(dados));
+
     }
 
 }
